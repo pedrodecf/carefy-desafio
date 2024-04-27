c55f8cfc850626bb24c65ad9cdb609aa001c5875
pedrodecf <pedro012014@gmail.com>
2024-04-27 01:50:31 -0300

    feat: estilos css

--- index.php
+++ index.php
@@ -1,11 +1,41 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Carefy Desafio</title>
+    <link rel="stylesheet" href="./css/index.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
+    <title>Lista de aniversário colaboradores carefy</title>
 </head>
+
 <body>
-    
+    <main>
+        <article class="container">
+            <header>
+                <h2>Próximos aniversários 💼</h2>
+                <span>Contribuintes que irão celebrar mais um ano de <b>empresa!</b></span>
+            </header>
+            <div class="aniversariante-container">
+                <div class="aniversariante-info">
+                    <p>Pedro Henrique</p>
+                    <span>03 de junho</span>
+                </div>
+                <span class="data-de-nascimento">1 ano</span>
+            </div>
+        </article>
+
+        <article class="container">
+            <header>
+                <h2>Aniversariante(s) do dia 💼</h2>
+                <span>Aniversariantes celebrando mais um ano de <b>empresa!</b></span>
+            </header>
+            <p class="sem-aniversariante">Ninguém fazendo aniversário hoje! ❌🥳</p>
+            </div>
+        </article>
+    </main>
 </body>
+
 </html>
