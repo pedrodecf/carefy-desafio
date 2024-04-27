e8a04b161e821d2ebab05906fc8307388da2f6c2
pedrodecf <pedro012014@gmail.com>
2024-04-27 15:18:30 -0300

    feat: controller para pegar dados da planilha

---
+++ controllers/AniversariosController.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once __DIR__ . '/../api/GoogleSheetsApi.php';
+
+$response = $service->spreadsheets_values->get($spreadsheetId, $range);
+$values = $response->getValues();
+
+if (empty($values)) {
+    print "Nenhum dado encontrado.\n";
+} else {
+    array_shift($values);
+
+    $employees = [];
+    foreach ($values as $row) {
+        $employee = [
+            'nome' => $row[0],
+            'data_de_nascimento' => $row[1],
+            'data_de_entrada' => $row[2],
+            'data_de_saida' => isset($row[3]) ? $row[3] : null,
+        ];
+        $employees[] = $employee;
+    }
+}
